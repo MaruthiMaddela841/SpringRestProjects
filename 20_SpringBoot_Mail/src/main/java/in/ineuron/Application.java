80e78429604b341292586a505a2575e050756175
Maruthi Maddela <maruthimaddela841@gmail.com>
2025-01-05 20:59:02 +0530

    updated

--- 20_SpringBoot_Mail/src/main/java/in/ineuron/Application.java
+++ 20_SpringBoot_Mail/src/main/java/in/ineuron/Application.java
@@ -14,9 +14,9 @@ public class Application {
 		ApplicationContext context = SpringApplication.run(Application.class, args);
 		IPurchaseOrder order = context.getBean(IPurchaseOrder.class);
 		try {
-			String msg = order.purchase(new String[] { "fossil-Chrono", "USPOLO_Tshirt", "Louis Phillipe" },
+			String msg = order.purchase(new String[] { "Fastrack-FASTRACK", "50,000-Hairdryer", "13 max pro" },
 					new double[] { 1200.0, 5000.0, 6000.0 },
-					new String[] {"maruthimaddela841@gmail.com","maruthimaddela517@gmail.com","maru.justin@gmail.com"});
+					new String[] {"maruthimaddelap1@gmail.com","shishira36@gmail.com","maru.justin@gmail.com"});
 			System.out.println(msg);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
